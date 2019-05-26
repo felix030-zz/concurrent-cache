7299b3c93ed24cb4106fa2bbc729cf33c5de49e7
Felix E. C. Klemke <me@felixklemke.com>
2019-05-26 10:50:28 +0200

    initial commit

---
+++ src/main/java/com/fklemke/concurrentcache/ConcurrentCacheApplication.java
@@ -0,0 +1,13 @@
+package com.fklemke.concurrentcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrentCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrentCacheApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fklemke/concurrentcache/ConcurrentCacheApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fklemke.concurrentcache;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConcurrentCacheApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
