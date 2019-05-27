852ae9573455e2a6eca1a31f24b47e4629e997cb
Felix E. C. Klemke <me@felixklemke.com>
2019-05-27 20:43:42 +0200

    CONCCCURRENT-CACHE: Remove useless interface

--- README.md
+++ README.md
@@ -17,10 +17,6 @@ locally, this path needs to be updated to the hosting system.
 The process method returns an empty CompletableFuture in order to flag when the processing is finished and signal the 
 PrintOutService that it can traverse the Cache to print out the statistics.
 
-##### WordParseService
-The service is a basic implementation and implements the custom `WordParser` interface. The purpose is to be easily 
-replaceable by a more sophisticated implementation.
-
 ##### Cache
 For this project the Hazelcast implementation of the JCache standard is used. Only methods defined in the JSR-107 are being
 used, it should therefore be easily replaceable by other implementations c.f.: [JSR-107 implementations](https://jcp.org/aboutJava/communityprocess/implementations/jsr107/index.html)
--- src/main/java/com/fklemke/concurrentcache/services/WordParseService.java
+++ src/main/java/com/fklemke/concurrentcache/services/WordParseService.java
@@ -1,6 +1,5 @@
 package com.fklemke.concurrentcache.services;
 
-import com.fklemke.concurrentcache.interfaces.WordParser;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -10,14 +9,14 @@ import java.util.List;
  * Class to handle parsing into native words.
  */
 @Service
-public class WordParseService implements WordParser {
+public class WordParseService {
 
     /**
      * Handles parsing of single lines of words into a List of lowercase words.
      * @param line the given text line.
      * @return a List of lowercase words.
      */
-    public List<String> getWordsFromTextLine(String line) {
+    List<String> getWordsFromTextLine(String line) {
         List<String> resultWordList = new ArrayList<>();
 
         //\p{L} or \p{Letter}: matches on any kind of letter from any language.
